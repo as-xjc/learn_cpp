ac59cdfc19c854ba1a4347c94a3f7d7f9360a2e4
as-xjc <azuresky.xjc@gmail.com>
2018-04-06 01:21:49 +0800

    update future

--- future.cpp
+++ future.cpp
@@ -4,19 +4,17 @@
 #include <functional>
 #include <iostream>
 
-using handler = std::function<bool()>;
-
-std::map<int, handler> handlers;
+std::map<int, std::function<bool()>> handlers;
 static int index_ = 0;
 
-template <typename RETURN, typename... ARGS>
-void async_run(std::function<RETURN(ARGS...)> call, std::function<void(RETURN)> cb) {
-  auto f = std::async(std::launch::async, call);
-  auto fu = new std::future<RETURN>(std::move(f));
-  auto check = [&call, cb, fu]() -> bool {
+template <typename RETURN>
+void async_run(std::function<RETURN()>&& call, std::function<void(RETURN)>&& cb) {
+  auto f = std::async(std::launch::async, std::move(call));
+  auto fu = std::make_shared<std::future<RETURN>>(std::move(f));
+  auto callcb = std::make_shared<std::function<void(RETURN)>>(std::move(cb));
+  auto check = [callcb, fu]() -> bool {
     if (fu->wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
-      cb(fu->get());
-      delete fu;
+      (*callcb)(fu->get());
       return true;
     }
     return false;
@@ -25,22 +23,40 @@ void async_run(std::function<RETURN(ARGS...)> call, std::function<void(RETURN)> 
   handlers.emplace(index_++, std::move(check));
 };
 
+template <typename RETURN>
+void async_run(RETURN(*call)(), void(*cb)(RETURN)) {
+  async_run(std::function<RETURN()>(call), std::function<void(RETURN)>(cb));
+}
+
+template <typename RETURN>
+void async_run(std::function<RETURN()>&& call, void(*cb)(RETURN)) {
+  async_run(std::move(call), std::function<void(RETURN)>(cb));
+}
+
+template <typename RETURN>
+void async_run(RETURN(*call)(), std::function<void(RETURN)>&& cb) {
+  async_run(std::function<RETURN()>(call), std::move(cb));
+}
+
 
 int call() {
   std::cout << std::time(nullptr) << " start sleep" << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  std::this_thread::sleep_for(std::chrono::seconds(10));
   std::cout << std::time(nullptr) << " end sleep" << std::endl;
   return 5;
 }
 
 void callcb(int i) {
-  std::cout << std::time(nullptr) << " callback:" << 1 << std::endl;
+  std::cout << std::time(nullptr) << " callback:" << i << std::endl;
 }
 
 int main() {
-
-  async_run(std::function<int(void)>(call), std::function<void(int)>(callcb));
+  async_run(call, callcb);
+  async_run(call, std::function<void(int)>([](int i) {
+    std::cout << std::time(nullptr) << " callback:" << i << std::endl;
+  }));
   std::this_thread::sleep_for(std::chrono::seconds(1));
+
   while (true) {
     std::cout <<std::time(nullptr) <<  " run a loop" << std::endl;
     for (auto it = handlers.begin(); it != handlers.end(); ) {
